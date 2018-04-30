f951f570d75fee5702f76cb426f1a6c1a7915ef9
BigEgg <cd_bigegg@hotmail.com>
2018-04-30 00:01:07 -0700

    Add logic to compute the probability (likely hood)

--- lab8/particle_filter.py
+++ lab8/particle_filter.py
@@ -19,9 +19,10 @@ def motion_update(particles, odom):
     for particle in particles:
         #   Move particle in the exact same way, but add noise to each movement
         (dx, dy, dh) = add_odometry_noise(odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA)
+        # (dx, dy, dh) = odom
         #   Rotate the *robot frame* location to *world frame*
         (dx, dy) = rotate_point(dx, dy, dh)
-        motion_particles.append(Particle(particle.x + dx, particle.y + dy, particle.h + odom[2]))
+        motion_particles.append(Particle(particle.x + dx, particle.y + dy, particle.h + dh))
     return motion_particles
 
 # ------------------------------------------------------------------------
@@ -49,7 +50,46 @@ def measurement_update(particles, measured_marker_list, grid):
         Returns: the list of particles represents belief p(x_{t} | u_{t})
                 after measurement update
     """
-    measured_particles = []
+    weights = []
+    has_weight = False
+    for particle in particles:
+        particle_measured_marker = particle.read_markers(grid)
+        likely_hood = 0
+        for measurement1 in measured_marker_list:
+            for measurement2 in particle_measured_marker:
+                likely_hood += measurement_prob(measurement1, measurement2)
+        if likely_hood != 0:
+            has_weight = True
+        weights.append(likely_hood)
+
+    #resample 90% of the particle, and add 10% random particle uni
+    resample_rate = 0.9
+    if not has_weight:
+        #   In some case their had no weight after the calculation
+        measured_particles = random.choices(particles, k=(int)(resample_rate * len(particles)))
+    else:
+        measured_particles = random.choices(particles, weights=weights, k=(int)(resample_rate * len(particles)))
+    measured_particles.extend(Particle.create_random((int)((1 - resample_rate) * len(particles)), grid))
+
     return measured_particles
 
+
+def measurement_prob(measurement1, measurement2):
+    (x0, y0, h0) = measurement1
+    (x1, y1, h1) = measurement2
+    (delta_x, delta_y, delta_h) = (x1 - x0, y1 - y0, h1 - h0)
+
+    dist = math.sqrt(delta_x ** 2 + delta_y ** 2)
+    return gaussian(0, MARKER_TRANS_SIGMA, delta_x) * gaussian(0, MARKER_TRANS_SIGMA, delta_y) * gaussian(0, MARKER_ROT_SIGMA, delta_h)
+
+
+def gaussian(mu, sigma, x):
+    # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+    # Probability density function:
+    #                                                         (x - mu) ^ 2
+    #                                                    (-) --------------
+    #                                   1                     2 *sigma ^ 2
+    # f(x | mu, sigma ^ 2) = ------------------------- e
+    #                        sqrt(2 * pi * sigma ^ 2)
+    return math.exp(- ((mu - x) ** 2) / (2 * sigma ** 2)) / math.sqrt(2.0 * math.pi * (sigma ** 2))
 
