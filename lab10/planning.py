4617c0c25025f6e9c8cdd956984f52254bca8c51
BigEgg <cd_bigegg@hotmail.com>
2018-05-05 21:26:17 -0700

    Add the priority queue for the A* algo

--- lab10/planning.py
+++ lab10/planning.py
@@ -4,6 +4,21 @@ import threading
 from queue import PriorityQueue
 import math
 import cozmo
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
 
 
 def astar(grid, heuristic):
